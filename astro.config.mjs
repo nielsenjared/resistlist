75f2d137cc3aca929f0da18f9afe6d89e73755cc
Jared Nielsen <nielsen.jared@gmail.com>
2025-05-17 09:56:59 -0400

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://resistlist.us',
+  base: '/resistlist', // Not needed if deploying to username.github.io
+  // other config options...
+});
